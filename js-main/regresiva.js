023875f1b4eb048d636c7eedfa324a5e0630b04a
Micael <micael.lucas.silva@gmail.com>
2021-10-01 02:15:45 -0300

    Atualização

--- js-main/regresiva.js
+++ js-main/regresiva.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("October 04, 2021 19:00:00").getTime();
+var countDownDate = new Date("October 15, 2021 23:00:00").getTime();
 
 //October 04, 2021 19:00:00
 // Update the count down every 1 second
